92698119b905f4123a8724e3e402957ade8afff8
Teddy Heinen <teddy@heinen.dev>
2021-12-01 00:27:39 -0600

    fix weird heap bug idk why it works

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 mod day1;
 
+use shmalloc::Shmeap;
+
+#[global_allocator]
+static ALLOCATOR: Shmeap<
+    0,
+    { 1 << 24 },
+    { libc::PROT_READ | libc::PROT_WRITE },
+    { libc::MAP_ANONYMOUS | libc::MAP_SHARED },
+    "",
+> = Shmeap::new();
 
 #[macro_use]
 extern crate aoc_runner_derive;
