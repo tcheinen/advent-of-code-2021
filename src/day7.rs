caaffc4c4df82dbf57e1c5c68ca4f539c1bd871d
Teddy Heinen <teddy@heinen.dev>
2021-12-07 11:43:05 -0600

    optimize day 7 lol

--- src/day7.rs
+++ src/day7.rs
@@ -6,26 +6,38 @@ use tap::prelude::*;
 #[aoc_generator(day7)]
 pub fn generator(input: &str) -> (i64, i64, Vec<i64>) {
     fn inner(input: Vec<i64>) -> (i64, i64, Vec<i64>) {
-        (*input.iter().min().unwrap(),*input.iter().max().unwrap(), input)
+        (
+            *input.iter().min().unwrap(),
+            *input.iter().max().unwrap(),
+            input,
+        )
     }
     input
         .split(",")
         .map(|x| str::parse(x).unwrap())
         .collect::<Vec<_>>()
         .pipe(inner)
-
 }
-
 
 #[aoc(day7, part1)]
 pub fn solve_part1((min, max, input): &(i64, i64, Vec<i64>)) -> i64 {
-    (*min..=*max).map(|target| input.iter().map(|x| (x - target).abs()).sum()).min().unwrap()
+    (*min..=*max)
+        .map(|target| input.iter().map(|x| (x - target).abs()).sum())
+        .min()
+        .unwrap()
 }
 
 #[aoc(day7, part2)]
 pub fn solve_part2((min, max, input): &(i64, i64, Vec<i64>)) -> i64 {
-    (*min..=*max).map(|target| input.iter().map(|x| (1..=(x - target).abs()).sum::<i64>()).sum()).min().unwrap()
-
+    (*min..=*max)
+        .map(|target| {
+            input
+                .iter()
+                .map(|x| ((x - target).abs() * ((x - target).abs() + 1)) / 2)
+                .sum()
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -47,10 +59,9 @@ mod tests {
         let provided = "16,1,2,0,4,2,7,1,2,14";
 
         assert_eq!(168, solve_part2(&generator(provided)));
-        // assert_eq!(
-        //     104822130,
-        //     solve_part2(&generator(include_str!("../input/2021/day7.txt")))
-        // );
-        // lmao this takes forever to run so i simply do not
+        assert_eq!(
+            104822130,
+            solve_part2(&generator(include_str!("../input/2021/day7.txt")))
+        );
     }
 }
